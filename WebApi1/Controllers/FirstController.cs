f8ad5f577ead1a6e2c0b917e6dba9ebc8e8e50d2
AliAhmed-PC <AliAhmed-PC@DESKTOP-T90JGBG>
2022-03-11 14:02:36 +0500

    Add project files.

---
+++ WebApi1/Controllers/FirstController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi1.Controllers
+{
+    [Route("api/[controller]")] //www.google.com/api/First/getName
+    [ApiController]
+    public class FirstController : ControllerBase
+    {
+        [HttpGet("getName")]
+        public string GetName()
+        {
+            return "Welcome to API Course.";
+        }
+
+        [HttpGet("getDepartment")]
+        public string GetDepartment()
+        {
+            return "Department = IT";
+        }
+
+        [HttpGet("getById/{id}")]
+        public string GetById(int id)
+        {
+            return "Your Id is: " + id;
+        }
+    }
+}
